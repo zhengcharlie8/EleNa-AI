17860d11eca78b3a34c76baba3274c5a0e021677
zhengcharlie8 <44006237+zhengcharlie8@users.noreply.github.com>
2020-11-07 06:25:08 -0500

    installed springboot

---
+++ src/backend/src/main/java/elenaBackend/demo/ElenaBackendApplication.java
@@ -0,0 +1,13 @@
+package elenaBackend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElenaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElenaBackendApplication.class, args);
+	}
+
+}
---
+++ src/backend/src/test/java/elenaBackend/demo/ElenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package elenaBackend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
